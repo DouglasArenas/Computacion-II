902496af4217f41040a587a77ff862233360e5fc
Douglas Arenas <douglasarenas71@gmail.com>
2021-08-18 00:27:04 -0300

    archivo servidor remoto

---
+++ tp8/remote_shell.py
@@ -0,0 +1,33 @@
+import socket
+import subprocess as sp
+
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = ""
+port = 9009
+
+serversocket.bind((host, port))
+
+serversocket.listen(5)
+
+clientsocket, addr = serversocket.accept()
+
+while True:
+    d = clientsocket.recv(1024)
+    msg = d.decode("utf8")
+    if d == "" or len(d) == 0:
+        print("Exit")
+        break
+    print(f"Dirección: {str(addr)}")
+    print(f"Recibido: {msg}")
+
+    command = sp.Popen([msg], shell=True, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+    stdout, stderr = command.communicate()
+
+    if command.returncode == 0:
+        resp = "OK \n"+stdout
+    elif command.returncode != 0:
+        resp = "ERROR \n"+stderr
+
+    clientsocket.send(resp.encode('utf8'))
