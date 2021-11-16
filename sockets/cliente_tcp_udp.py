8aaa38517fce0da1be0fb28c6a4e7c39b10c125d
Douglas <douglasarenas71@gmail.com>
2021-11-16 13:27:21 -0300

    ejercicio 14

---
+++ sockets/cliente_tcp_udp.py
@@ -0,0 +1,33 @@
+import sys, socket, getopt
+
+if __name__ == '__main__':
+    (opt, arg) = getopt.getopt(sys.argv[1:], "a:p:t:")
+    for op, ar in opt:
+        if op == "-a":
+            host = int(ar)
+        if op == "-p":
+            port = int(ar)
+        if op == "-t":
+            protocol = str(ar)
+    
+    if protocol == "tcp":
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, port))
+        while True:
+            try:
+                content = input("Ingrese contenido: ")
+                client.send(content.encode('ascii'))
+            except EOFError:
+                print("Cliente desconectado")
+                client.close()
+                sys.exit()
+    
+    elif protocol == "udp":
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while True:
+            try:
+                content = input("Ingrese contenido: ")
+                client.sendto(content.encode('ascii'), (host, port))
+            except EOFError:
+                print("Saliendo...")
+                sys.exit()
--- sockets/server_tcp_udp.py
+++ sockets/server_tcp_udp.py
@@ -1,100 +1,47 @@
-import sys
-import socket
-import getopt
-import signal
+import sys, socket, getopt
 
-
-signal.signal(signal.SIGINT, exit)
-
-try:
-    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:t:f:')
-except getopt.GetoptError as err:
-    print(err)
-    sys.exit(2)
-
-try:
+if __name__ == '__main__':
+    (opt, arg) = getopt.getopt(sys.argv[1:], "p:t:f:")
     for (op, ar) in opt:
-        if (op in ['-p']):
+        if op == '-p':
             port = int(ar)
-        elif (op in ['-t']):
+        if op == '-t':
             protocol = ar
-        elif (op in ['-f']):
-            pathfile = ar
-        else:
-            sys.exit(2)
-except ValueError:
-    print("El argumento de -p debe ser un numero")
-    sys.exit()
+        if op == '-f':
+            path = ar
+    
+    if protocol == 'tcp':
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = ""
+        server.bind((host, port))
+        server.listen(5)
+        print(host, port)
+        print("Servidor esperando conexiones...")
+        client, addr = server.accept()
+        while True:
+            file = open(path, 'a')
+            content = client.recv(1024)
+            file.write(content.decode('ascii') + "\n")
+            if content == '':
+                print ("Saliendo...")
+                break
+            print("Mensaje recibido")
+        client.close()
 
-if protocol == 'tcp':
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif protocol == 'udp':
+        server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        host = ""
+        server.bind((host, port))
+        print("Servidor esperando conexiones...")
+        while True:
+            file = open(path, 'a')
+            content, addr = server.recvfrom(1024)
+            file.write(content.decode('ascii') + "\n")
+            if content == '':
+                print ("Saliendo...")
+                break
+            print("Mensaje recibido")
 
-    host = ""
-
-    try:
-        serversocket.bind((host, port))
-    except NameError:
-        print("Puerto no definido")
-        print("Utilice -p para definir el puerto")
+    else:
+        print("Protocolo invalido\nSaliendo...")
         sys.exit()
-    except OverflowError:
-        print("El puerto debe ser entre 0-65535")
-        sys.exit()
-    except PermissionError:
-        print("Permiso denegado")
-        sys.exit()
-
-    serversocket.listen(5)
-    print("Esperando conexiones")
-
-    clientsocket, addr = serversocket.accept()
-
-    while True:
-        f = open(pathfile, "a")
-        d = clientsocket.recv(1024)
-        msg = d.decode("ascii")
-        f.write(msg+"\n")
-        if d == "" or len(d) == 0:
-            print("Exit")
-            break
-        print(f"Direccion: {str(addr)}")
-        print(f"Recibido: {msg}")
-
-    clientsocket.close()
-
-elif protocol == 'udp':
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    host = ""
-
-    try:
-        serversocket.bind((host, port))
-    except NameError:
-        print("Puerto no definido")
-        print("Utilice -p para definir el puerto")
-        sys.exit()
-    except OverflowError:
-        print("El puerto debe ser entre 0-65535")
-        sys.exit()
-    except PermissionError:
-        print("Permiso denegado")
-        sys.exit()
-
-    print("Esperando conexiones")
-
-    while True:
-        f = open(pathfile, "a")
-        d, addr = serversocket.recvfrom(1024)
-        msg = d.decode("ascii")
-        f.write(msg+"\n")
-        address = addr[0]
-        port = addr[1]
-        if d == "" or len(d) == 0:
-            print("Exit")
-            break
-        print(f"Dirección: ('{str(address)}', {str(port)})")
-        print(f"Recibido: {msg}")
-
-else:
-    print("Protocolo invalido")
-    sys.exit()
